7297f8abf3519d2f9bc0c8b3cf286cd3689f0079
Nathanael Liechti <technat@technat.ch>
2023-09-15 13:44:26 +0200

    feat: remove chezmoi line

--- cloud-script.sh
+++ cloud-script.sh
@@ -2,5 +2,3 @@
 # Note: script is executed as root
 echo "Hello World! I'm starting up now at $(date -R)!"
 echo "Script is running as user $(whoami)"
-
-su ${username} -c "sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply the-technat"
