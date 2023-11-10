1807121850e9ca5b381d85d529d145103300e84c
Nathanael Liechti <technat@technat.ch>
2023-11-10 14:07:48 +0000

    feat: expose on public net

--- tevbox.tf
+++ tevbox.tf
@@ -36,7 +36,7 @@ resource "tailscale_tailnet_key" "bootstrap" {
   reusable      = false
   preauthorized = true
   expiry        = 300 # 5min
-  tags          = ["tag:feature-funnel"]
+  tags          = []
 }
 
 ############
