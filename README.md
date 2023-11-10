8ea930cb43d4e4483646c4b9e3b80c4e5c461a86
Nathanael Liechti <technat@technat.ch>
2023-11-10 12:57:42 +0000

    feat: make tevbox multi-account ready

--- README.md
+++ README.md
@@ -4,24 +4,25 @@ Technat's Development Box in the cloud
 
 ## Goal
 
-From time to time we need a development environment on the go. While there are many solutions out there, I prefer to setup my own. This has multiple reasons:
+From time to time we need a development environment on the go. While there are many solutions out there for this exact problem, I prefer to setup my own. This has multiple reasons:
 - performance of cloud-based IDEs is often rather poor or insufficient (in my personal experience)
 - control of the network stack is often not given (e.g no public IP or direct access to the machines NIC which is sometimes nice to have)
 - they usually stop working when you are inactive for some time (e.g they are session-based)
+- you have no idea what's all running on your IDE machine
 
 So this repo provides a solution how you can quickly create a fresh cloud server ready for you to code and tinker on.
 
 ## Solution design
 
-In general: a tevbox is just a dead-simple VPS with a mainstream Linux Distro and some development tools as well as the famous [code-server](https://github.com/coder/code-server). All other tools you need to develop are most likely also browser-based so this means you can code from wherever you want. In my case I'm using [Hetzner](http://hetzner.de/) for this job as their machines are very affordable. But it could be anything, doesn't even need to be public (but Internet access would help ;))
+In general: a tevbox is just a dead-simple VPS with a mainstream Linux Distro and some development tools as well as the famous [code-server](https://github.com/coder/code-server). All other tools you need to develop are most likely also browser-based so this means you can code from wherever you want. In my case I'm using [Hetzner](http://hetzner.de/) for this job as their machines are very affordable. But it could be anything, doesn't even need to be public, due to the awesome work of [Tailscale](https://tailscale.com).
 
 The workflow for creating a new VM is as follows:
 - Head over to the Actions tab of this repository and manually dispatch the workflow "Create new instance".
-  - You are asked certain parameters which are sometimes mandatory and sometimes just to allow for customization
+  - You are asked certain parameters which are sometimes mandatory and sometimes just to allow for customization in special situations
   - Some settings are also read from your Github user (since the workflow knows who initiated it)
 - The workflow will pass these variables 1:1 to [Terraform](https://www.terraform.io/) which in turn creates the server
   - Terraform is stateless, so we fire and forget which makes the machine unmanaged afterwards (e.g state is deleted after every run)
-  - Terraform uses a dead-simple cloud-init script to execute `ansible-pull` to bootstrap the server at first boot
+  - Terraform uses a dead-simple cloud-init script which executes `ansible-pull` to bootstrap the server at first boot
 - Ansible itself is executed as root (due to cloud-init), so by default all things will be done by root
   - Ansible receives it's dynamic variables from Terraform which has templated the `ansible-pull` command with CLI variables -> they will always override any other variables set within ansible
   - Ansible will bootstrap the server, in case of a failure, it will abort or never run if there is a syntax error. In such a scenario the only way to get access to the server is using the `rootkey` defined in the project
@@ -29,10 +30,10 @@ The workflow for creating a new VM is as follows:
 - To delete the machine you can simply trigger the workflow "Delete an instance" and enter the name of your instance
 
 Here are some things to note when working on the machine:
-- ssh keys are automatically copied from your Github user
-- the machines has joined my tailnet called 'alleaffengaffen.org.github' and thus has access to other machines in this VPN
+- Your user is the same as your github user, ssh keys are automatically copied 
+- the box joines a tailnet and thus has access to other machines in this VPN
 - the code-server is running for your specific user and accessible over the funnel with the password of your username
-- the tailscale [funnel]() on port 443 is already blocked due to the code-server 
+- the tailscale [funnel](https://tailscale.com/kb/1223/funnel/) on port 443 is already blocked due to the code-server 
 - ufw firewall is enabled, blocking all incoming traffic on the public IP except SSH -> you need to open ports you want to use
 - authentication against Github is done automatically if you are using the code-server in a browser (done using device auth)
 
@@ -45,8 +46,11 @@ For the tevbox project to work, it's important to have some static things:
   - An SSH key named `rootkey` which allows emergency access to the root user (if ansible doesn't reconfiure ssh) + prevents Hetzner from sending a mail with the generated root password
 - A tailnet with:
   - A tailscale API key to generate tailnet keys saved as repository secret -> note that this key will expire in 90 days 
-  - At least a tag named `funnel` that adds the funnel attribute to the nodes
+  - A tag named `funnel` that adds the funnel attribute to the nodes
+  - A tag named `tevbox` that grants the tevbox the desired permissions
 - This repo holding the source-code
+
+Note: currently I have hardcoded the option to switch between `root` and `alleaffengaffen` when it comes to the tailnet/account to work with. But this separation is only done within the workflow.
 
 ## Open Ideas
 
--- tevbox.tf
+++ tevbox.tf
@@ -1,52 +1,92 @@
 ############
+# Resources
+############
+resource "hcloud_server" "tevbox" {
+  name        = var.hostname
+  image       = var.image
+  server_type = var.type
+  location    = var.location
+  keep_disk   = true
+  ssh_keys    = ["rootkey"] # there must be a key named rootkey in the Hcloud project to prevent Hetzner sending us mails with the root password
+  public_net {
+    ipv4_enabled = true # as soon as github.com supports ipv6, this is theoretically not needed any more
+    ipv6_enabled = true
+  }
+  user_data = <<EOT
+    #cloud-config ${var.hostname}
+    packages:
+    - python3
+    - python3-pip
+    - git
+    runcmd:
+      - |
+        pip3 install ansible
+        ansible-galaxy install collection community.general
+
+        ansible-pull -C develop --clean --purge -i localhost, \
+        -U https://github.com/alleaffengaffen/tevbox.git \
+        -vv tevbox.yml -e username=${var.username} \
+        -e password=${var.password} -e ssh_port=${var.ssh_port} \
+        -e ts_auth_key=${tailscale_tailnet_key.bootstrap.key}
+  EOT
+}
+
+resource "tailscale_tailnet_key" "bootstrap" {
+  ephemeral     = false
+  reusable      = false
+  preauthorized = true
+  expiry        = 300 # 5min
+  tags          = ["tag:funnel", "tag:tevbox"]
+}
+
+############
+# Data Sources
+############
+data "tailscale_device" "tevbox" {
+  name     = "${var.hostname}.${var.tailnet_domain}"
+  wait_for = "300s"
+
+  depends_on = [hcloud_server.tevbox]
+}
+
+############
 # Variables
 ############
 variable "hostname" {
   type        = string
-  default     = ""
-  description = "Hosname of the instance (if empty will be generated)"
 }
 
 variable "username" {
   type        = string
-  default     = "technat"
-  description = "Admin user to create"
 }
 
 variable "password" {
   type        = string
   sensitive   = true
-  default     = ""
-  description = "Password for user created by cloud-init (empty means it's disabled)"
-}
-
-variable "github_user" {
-  type        = string
-  description = "Username of the user that initiated the Workflow"
 }
 
 variable "ssh_port" {
   type        = number
-  default     = 22
-  description = "SSH port to configure"
 }
 
-variable "instance_type" {
+variable "type" {
   type        = string
-  default     = "a1-ram2-disk20-perf1"
-  description = "Size of the instance to create"
 }
 
-variable "flavor" {
+variable "image" {
   type        = string
-  default     = "ubuntu-22.04"
-  description = "OS Flavor"
 }
 
 variable "location" {
   type        = string
-  default     = "hel1"
-  description = "Location to deploy the server to"
+}
+
+variable "tailnet" {
+  type = string
+}
+
+variable "tailnet_domain" {
+  type = string
 }
 
 # these vars are filled by GH action secrets
@@ -63,31 +103,31 @@ variable "tailscale_api_key" {
 # Outputs
 ############
 output "hostname" {
-  value = local.hostname
+  value = var.hostname
 }
 
 output "username" {
   value = var.username
 }
 
-output "password" {
-  value = nonsensitive(var.password)
-}
-
 output "ssh_port" {
   value = var.ssh_port
 }
 
-output "instance_type" {
-  value = var.instance_type
+output "type" {
+  value = var.type
 }
 
 output "flavor" {
-  value = var.flavor
+  value = var.image
 }
 
 output "location" {
   value = var.location
+}
+
+output "tailnet" {
+ value = var.tailnet
 }
 
 output "public_ipv4" {
@@ -103,75 +143,7 @@ output "tailscale_addresses" {
 }
 
 output "code_server_url" {
-  value = "https://${local.hostname}.${local.tailnet_domain}/"
-}
-
-############
-# Resources
-############
-locals {
-  tailnet_domain  = "little-cloud.ts.net"
-  tailnet         = "alleaffengaffen.org.github"
-  hostname        = var.hostname != "" ? var.hostname : "tevbox-${random_integer.count.result}"
-  root_ssh_key_id = "rootkey" # there must be a key named rootkey in the Hcloud project to prevent Hetzner sending us mails with the root password
-}
-resource "hcloud_server" "tevbox" {
-  name        = local.hostname
-  image       = var.flavor
-  server_type = var.instance_type
-  location    = var.location
-  keep_disk   = true
-  ssh_keys    = [local.root_ssh_key_id]
-  public_net {
-    ipv4_enabled = true # as soon as github.com supports ipv6, this is theoretically not needed any more
-    ipv6_enabled = true
-  }
-  user_data = <<EOT
-    #cloud-config <hostname>
-    packages:
-    - python3
-    - python3-pip
-    - git
-    runcmd:
-      - |
-        %{if var.flavor == "rocky-9"}
-        sudo dnf install epel-release tar -y 
-        sleep 2
-        sudo dnf install ufw -y
-        %{endif}
-        pip3 install ansible
-        git clone https://github.com/alleaffengaffen/tevbox.git /root/tevbox
-        ansible-galaxy install -r /root/tevbox/requirements.yml
-        rm -rf /root/tevbox
-        ansible-pull -C develop --clean --purge -i localhost, \
-        -U https://github.com/alleaffengaffen/tevbox.git \
-        -vv tevbox.yml -e username=${var.username} \
-        -e password=${var.password} -e github_user=${var.github_user} \
-        -e ssh_port=${var.ssh_port} -e ts_auth_key=${tailscale_tailnet_key.bootstrap.key}
-  EOT
-}
-
-resource "tailscale_tailnet_key" "bootstrap" {
-  ephemeral     = false
-  reusable      = false
-  preauthorized = true
-  expiry        = 300 # 5min
-  tags          = ["tag:funnel", "tag:lan"]
-}
-
-resource "random_integer" "count" {
-  min = 1
-  max = 100
-}
-
-############
-# Data Sources
-############
-data "tailscale_device" "tevbox" {
-  name     = "${local.hostname}.${local.tailnet_domain}"
-  wait_for = "300s"
-
-  depends_on = [hcloud_server.tevbox]
+  value = "https://${var.hostname}.${var.tailnet_domain}/"
 }
 
 ############
@@ -183,19 +155,16 @@ provider "hcloud" {
 
 provider "tailscale" {
   api_key = var.tailscale_api_key # expires every 90 days
-  tailnet = local.tailnet         # created by sign-up via Github
+  tailnet = var.tailnet         # created by sign-up via Github
 }
 
 terraform {
-  required_version = ">= 1.5.6"
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.42.1"
     }
     tailscale = {
       source  = "tailscale/tailscale"
-      version = "~> 0.13.9"
     }
   }
 }
